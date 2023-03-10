59ca8367c3f07a347f926fbabf45402eb2419620
Cheespeasa1234 <45641361+Cheespeasa1234@users.noreply.github.com>
2023-03-10 08:50:53 -0500

    fix links

---
+++ blog/script.js
@@ -0,0 +1,58 @@
+posts = [
+  {
+    title: "The first entry.",
+    date: "March 11, 2022, 12:25",
+    content: /* html */ `
+        <p>
+            This is the first entry. IDK what to put here. I might put a blog. After all, I did name this file blogtest.html. I kind of have an obligation now. Maybe it will be a coding blog.
+        </p>
+        <p>
+            So lets start, then? I figured out subpages just now. Funny, cause my previous website didn't use those. Turns out it's just as easy as making subfolders. Next task, getting rid of the "something.html" on the nav. I want it to look clean. Lets see if I can figure that out. I also want to make an icon and a title in the tab. That would be cool.
+        </p>
+        <p>
+            Today, I have:
+        </p>
+        <ol>
+            <li>Made a subpage</li>
+            <li>Made a hub</li>
+            <li>Made this site</li>
+            <li>Fallen asleep during class</li>
+        </ol>
+        <p>
+            I know. Very productive. The goal of this, btw, is not to flex my skill, but just to flex my non-skill. I'm pretty bad at this, so I just want to see how far I can get before I die. Pretty cool. Alright goodbye
+        </p>
+    `,
+  },
+  {
+    title: "Website Cleanup",
+    date: "March 24, 2022, 8:29",
+    content: /* html */ `
+        <p>
+            So I removed the file name from the url up top! If you look at the url, now it says /blog, not /blog/index.html. Very proud of that. Also, what I did with wordle might be illegal. I don't really care though. Also, I kind of erased all of the curse word from the site. Just in case. Hello Mr Zeller
+        </p>
+        <p>
+            Yesterday, I presented the pong game I made. I am going to see if chrome still supports applets. I don't think they do, so I'm going to try to find a way to upload the pong game I made to this site. It's really cool, and it features Walter White.
+        </p>
+        <p>
+            I added an about me page now. I put my email. Please don't email me unless it's important, that is legitimately my only email account that I can use, please don't blow it up. Who am I kidding, no one reads this. I added some of my achievements onto that page. Now that I'm thinking about that, I really haven't done much. Hopefully this site helps.
+        </p>
+        <p>
+            I don't know what next project to start on here. My current project has been written entirely in Java and Idon't know how to make Java go on a site. But I will return with more information on that.
+        </p>
+        <p>
+            Lastly, I wanted to add some information on my current workflow. Right now, I do pretty much everything for this website in vscode and then drag and drop in into the file manager and then it works. I might even buy premium to make this faster but then I'd need to generate a return from this site. However, I can't do that. Because then I'd be making money off of Wordle, nytimes' creation. That's illegal. I don't really care but I don't like the idea of a conglomerate megacorporation having it out for me. Although my allowance might cover it?
+        </p>
+        <p>
+            Thats it.
+        </p>
+        <p>
+            Have a good day gamers
+        </p>
+    `,
+  },
+  {
+    title: "WordleGen fixed, pme++ updooted, blog went bye bye",
+    date: "April 18, 2022, 8:57",
+    content: /* html */ ``
+  },
+];
