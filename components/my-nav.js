044820f79d28e08e7d2c2c8ab3ec3c4368a1e809
Cheespeasa1234 <45641361+Cheespeasa1234@users.noreply.github.com>
2023-03-15 11:35:27 -0400

    LOGO ON NAV, favicon (again), and titles to pages

--- components/my-nav.js
+++ components/my-nav.js
@@ -2,14 +2,15 @@ let a=(active)=>{
   class MyNav extends HTMLElement {
     connectedCallback() {
       this.innerHTML = /*html*/`
+      <img class="logo" alt="logo" src="${ROOT}img/logo.png">
       <nav>
-        <div>
-          <a class="first-link nav-link${active[0]?' active':''}" href="${ROOT}">Home</a>
-          <a class="nav-link${active[1]?' active':''}" href="${ROOT}about">About</a>
-          <a class="nav-link${active[2]?' active':''}" href="${ROOT}blog">Blog</a>
-          <a class="nav-link${active[3]?' active':''}" href="${ROOT}my_projects">Projects</a>
-        </div>
-        <div>
+      <div>
+      <a class="first-link nav-link${active[0]?' active':''}" href="${ROOT}">Home</a>
+      <a class="nav-link${active[1]?' active':''}" href="${ROOT}about">About</a>
+      <a class="nav-link${active[2]?' active':''}" href="${ROOT}blog">Blog</a>
+      <a class="nav-link${active[3]?' active':''}" href="${ROOT}my_projects">Projects</a>
+      </div>
+      <div>
           <a class="nav-link scl" href="https://www.github.com/Cheespeasa1234" target="_blank">
             <i class="fa-brands fa-github"></i>
           </a>
