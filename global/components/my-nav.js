42541899a56866c23f98fe79097b63ce5beec5ff
Nate Levison <natelevison@gmail.com>
2023-05-05 22:33:28 -0400

    rework links. components into global dir

--- global/components/my-nav.js
+++ global/components/my-nav.js
@@ -1,14 +1,14 @@
-let a=(active)=>{
+let loadNav=(activeLinks)=>{
   class MyNav extends HTMLElement {
     connectedCallback() {
       this.innerHTML = /*html*/`
       <img class="logo" alt="logo" src="${ROOT}img/logo.png">
       <nav>
       <div>
-      <a class="first-link nav-link${active[0]?' active':''}" href="${ROOT}">Home</a>
-      <a class="nav-link${active[1]?' active':''}" href="${ROOT}about">About</a>
-      <a class="nav-link${active[2]?' active':''}" href="${ROOT}blog">Blog</a>
-      <a class="nav-link${active[3]?' active':''}" href="${ROOT}my_projects">Projects</a>
+      <a class="first-link nav-link${activeLinks[0]?' active':''}" href="${ROOT}">Home</a>
+      <a class="nav-link${activeLinks[1]?' active':''}" href="${ROOT}about">About</a>
+      <a class="nav-link${activeLinks[2]?' active':''}" href="${ROOT}blog">Blog</a>
+      <a class="nav-link${activeLinks[3]?' active':''}" href="${ROOT}my_projects">Projects</a>
       </div>
       <div>
           <a class="nav-link scl" href="https://www.github.com/Cheespeasa1234" target="_blank">
