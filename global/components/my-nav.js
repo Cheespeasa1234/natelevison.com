8e538020f8fd95d45651e59e51a1a6eeb1e34c71
Nate Levison <natelevison@gmail.com>
2023-07-25 20:30:00 -0400

    custom element params

--- global/components/my-nav.js
+++ global/components/my-nav.js
@@ -1,14 +1,22 @@
-let loadNav=(activeLinks)=>{
-  class MyNav extends HTMLElement {
+class MyNav extends HTMLElement {
     connectedCallback() {
-      this.innerHTML = /*html*/`
+        let selected = this.getAttribute("selected-index");
+        this.innerHTML = /*html*/ `
       <img class="logo" alt="logo" src="${ROOT}img/logo.png">
       <nav>
       <div>
-      <a class="first-link nav-link${activeLinks[0]?' active':''}" href="${ROOT}">Home</a>
-      <a class="nav-link${activeLinks[1]?' active':''}" href="${ROOT}about">About</a>
-      <a class="nav-link${activeLinks[2]?' active':''}" href="${ROOT}blog">Blog</a>
-      <a class="nav-link${activeLinks[3]?' active':''}" href="${ROOT}my_projects">Projects</a>
+      <a class="first-link nav-link${
+          selected == 0 ? " active" : ""
+      }" href="${ROOT}">Home</a>
+      <a class="nav-link${
+          selected == 1 ? " active" : ""
+      }" href="${ROOT}about">About</a>
+      <a class="nav-link${
+          selected == 2 ? " active" : ""
+      }" href="${ROOT}blog">Blog</a>
+      <a class="nav-link${
+          selected == 3 ? " active" : ""
+      }" href="${ROOT}my_projects">Projects</a>
       </div>
       <div>
           <a class="nav-link scl" href="https://www.github.com/Cheespeasa1234" target="_blank">
@@ -24,6 +32,5 @@ let loadNav=(activeLinks)=>{
       </nav>
     `;
     }
-  }
-  customElements.define('my-nav', MyNav);
 }
+customElements.define("my-nav", MyNav);
---
+++ my_projects/java/script.js
@@ -0,0 +1,40 @@
+// define the custom Project element
+
+class ProjectElement extends HTMLElement {
+    render() {
+        let title = this.getAttribute("proj-title") || "no title given";
+        let timerange = this.getAttribute("proj-time") || "no timerange given";
+        let linkhref = this.getAttribute("proj-href") || undefined;
+        let linkname = this.getAttribute("proj-linkname") || undefined;
+        let desc = this.innerHTML || "no innerhtml found";
+
+        let link = `<a href="${linkhref}" class="project-lnk">${linkname}</a>`;
+
+        this.innerHTML = /* html */ `
+            <div class="project-head">
+                <span class="project-title">${title}</span>
+                <span class="project-time-range">${timerange}</span>
+                ${linkhref == undefined ? "no link" : link}
+            </div>
+            </p class="project-desc">${desc}</p>
+            asdasdasd
+        `;
+    }
+    connectedCallback() {
+        if(!this.rendered) {
+            this.render();
+            this.rendered = true;
+        }
+        
+    }
+    
+    attributedChangedCallback(name, oldValue, newValue) {
+        this.render()
+    }
+
+    static get observedAttributes() {return ["proj-title", "proj-time", "proj-href", "proj-linkname"]}
+    
+
+}
+
+customElements.define("my-proj", ProjectElement)
