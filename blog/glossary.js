acbd8942680eb7e23d5db85ea865236bbd19b55b
Cheespeasa1234 <45641361+Cheespeasa1234@users.noreply.github.com>
2023-09-06 06:59:52 -0400

    minesw post, bugs ig i forgor

--- blog/glossary.js
+++ blog/glossary.js
@@ -1,17 +1,18 @@
 const glossary = {
-    "backend": "The backend is the non-user-facing part of a website responsible for server-side tasks like data storage, payment processing, and email handling.",
-    "frontend": "The frontend is the user-facing part of a website, responsible for displaying content such as text, images, and videos on the user's device.",
+    "backend": "The backend is the part of a website that the user does not see. It may handle payments, emails, and authorization.",
+    "frontend": "The frontend is the part of a website that the user interacts with. It may be a mobile app, a website, or a web app.",
     "hosting": "Hosting is a service that runs backend code, relieving users of server management and often utilizing cloud technology for efficiency.",
-    "port forwarding": "Port forwarding enables public internet traffic to access a server and establish connections by directing incoming requests to specific ports.",
-    "server": "A server is either a physical machine or software that executes backend code, serving data and resources to users, often hosted in homes or the cloud.",
+    "port forwarding": "Port Forwarding allows those with personal computers to respond to public internet traffic, bypassing their firewall restrictions.",
+    "server": "A server is the physical machine / computer that executes backend code and is responsible for site traffic. It may or may not be running with server software.",
     "domain": "A domain name is the human-readable text associated with a website, representing the full IP address of the server where the site is hosted.",
-    "dns": "DNS, or Domain Name Service, is a nonprofit organization that maintains domain name records, allowing browsers to resolve domain names to IP addresses.",
+    "dns": "DNS, or Domain Name Service, is a nonprofit organization that has a phone book of IP addresses, allowing users to type in a website domain instead of an IP every time.",
     "ssh": "SSH, or Secure Shell Protocol, enables remote server access through a secure command-line interface, providing a secure way to manage servers.",
-    "printer": "A printer is a device that produces printed documents by transferring ink or toner onto paper, though it is notorious for its tendency to malfunction.",
+    "printer": "A printer is a fragile and mysterious work of magic, its inner functions far too complex for any form of life on Earth to grasp.",
     "encryption": "Encryption is a method of securing data during transmission over the internet, rendering it unreadable to anyone without the decryption key.",
     "homebrewed": "Homebrewed refers to creating something without relying on external services or libraries, often implying a DIY or custom-made approach.",
     "authorization": "Authorization involves confirming a user's identity through encryption methods, including verification, passwords, and two-factor authentication (2FA).",
     "nginx": "Nginx is a versatile server software that efficiently manages multiple backends on a single server, suitable for various tasks, including reverse proxying.",
+    "express": "Express is a simple server software that allows routing and middleware connections to be made much easier.",
     "debugging": "Debugging is the process of identifying and resolving issues in code when it does not perform as intended, ensuring it functions correctly.",
     "ssd": "An SSD, or Solid-State Drive, is a storage device that uses semiconductor technology, offering faster data access and durability compared to traditional HDDs.",
     "boids simulation": "A Boids simulation models emergent behavior, demonstrating how simple rules governing individual agents can lead to complex collective behavior, like bird flocking or fish schooling.",
@@ -22,7 +23,8 @@ const glossary = {
     "css": "CSS, or Cascading Style Sheets, is a widely used language for styling websites, allowing designers to control elements' appearance, including colors, sizes, and layouts.",
     "javascript": "JavaScript is the dominant programming language for web development, enabling interactive features like animations, games, and dynamic content.",
     "js": "JS is a common abbreviation for JavaScript, the scripting language widely used in web development for enhancing website interactivity and functionality.",
-    "html": "HTML, or HyperText Markup Language, defines the structure and content of web pages, serving as the foundation for web document rendering in browsers."
+    "html": "HTML, or HyperText Markup Language, defines the structure and content of web pages, serving as the foundation for web document rendering in browsers.",
+    "tech stack": "A tech stack is the set of server softwares and tools in use by a website. A tech stack includes frontend softwares, databases, and compilers as well."
 }
 
 function glossarize(element) {
