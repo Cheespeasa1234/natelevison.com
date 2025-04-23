9b2e599ee0f8bd10e5b0623b96f5175eb03c3870
Nate Levison <45641361+Cheespeasa1234@users.noreply.github.com>
2025-04-23 00:19:04 +0000

    add sitemaps and sitemap makers

---
+++ src/scripts/genBlogXml.ts
@@ -0,0 +1,56 @@
+import { getAllBlogNames, getBlogArticle } from "../lib/blog";
+import fs from "fs";
+
+function getSitemapString(): string {
+    const articleNames = getAllBlogNames();
+    const articles = articleNames.map(getBlogArticle);
+    
+    const xmlEntries: string[] = [];
+    
+    for (const article of articles) {
+        const unlisted = article.info.unlisted;
+        if (unlisted) continue;
+
+        const created = article.info.created;
+        const name = article.info.name;
+        const url = article.content.url;
+        const starred = article.info.starred;
+        const type = article.content.type;
+
+        const fullUrl = `https://natelevison.com${type === 'html' ? "/" : ""}${url}`;
+        console.log(fullUrl);
+
+        const lastMod = new Date(created).toISOString();
+        console.log(lastMod);
+        console.log()
+
+        const priority = starred ? 0.75 : 0.5;
+
+        const xmlEntry = `
+    <url>
+        <loc>${fullUrl}</loc>
+        <lastmod>${lastMod}</lastmod>
+        <priority>${priority}</priority>
+    </url>`;
+
+        xmlEntries.push(xmlEntry);
+    }
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">${xmlEntries.join('')}
+</urlset>
+    `;
+
+    return sitemap;
+}
+
+function writeSitemapToFile(sitemap: string) {
+    const path = "static/sitemaps/blog.xml";
+    fs.writeFileSync(path, sitemap);
+}
+
+function main() {
+    writeSitemapToFile(getSitemapString());
+}
+
+main();
