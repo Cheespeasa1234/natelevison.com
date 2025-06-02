1e4e0f5d30ce3f3b62219c72f81df400bfef1827
Nate Levison <45641361+Cheespeasa1234@users.noreply.github.com>
2025-06-02 14:28:15 +0000

    download static assets to make it faster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -51,15 +51,11 @@
 
 <svelte:head>
     <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
+        href="/bootstrap-5.3.6-dist/css/bootstrap.min.css"
         rel="stylesheet"
-        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
-        crossorigin="anonymous"
     />
     <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
-        crossorigin="anonymous"
+        src="/bootstrap-5.3.6-dist/js/bootstrap.bundle.min.js"
     ></script>
     <meta name="author" content="Nate Levison" />
 
